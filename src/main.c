eeb5cb0bf8db419dd4b81e37d14c939d1e611172
angelcet <coolet.lin@gmail.com>
2019-03-06 17:53:29 +0800

    Update firmware version 1.11.01

---
+++ src/main.c
@@ -0,0 +1,112 @@
+/**
+ * Copyright (c) 2014 - 2018, Nordic Semiconductor ASA
+ * 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form, except as embedded into a Nordic
+ *    Semiconductor ASA integrated circuit in a product or a software update for
+ *    such product, must reproduce the above copyright notice, this list of
+ *    conditions and the following disclaimer in the documentation and/or other
+ *    materials provided with the distribution.
+ * 
+ * 3. Neither the name of Nordic Semiconductor ASA nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ * 
+ * 4. This software, with or without modification, must only be used with a
+ *    Nordic Semiconductor ASA integrated circuit.
+ * 
+ * 5. Any software provided in binary form under this license must not be reverse
+ *    engineered, decompiled, modified and/or disassembled.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY NORDIC SEMICONDUCTOR ASA "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL NORDIC SEMICONDUCTOR ASA OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
+/** @file
+ *
+ * @defgroup ble_sdk_app_template_main main.c
+ * @{
+ * @ingroup ble_sdk_app_template
+ * @brief Template project main file.
+ *
+ * This file contains a template for creating a new application. It has the code necessary to wakeup
+ * from button, advertise, get a connection restart advertising on disconnect and if no new
+ * connection created go back to system-off mode.
+ * It can easily be used as a starting point for creating a new application, the comments identified
+ * with 'YOUR_JOB' indicates where and how you can customize.
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+
+#include "common_service.h"
+#include "fstorage_service.h"
+#include "app_usbd_21.h"
+
+extern bool option_pin_mode0;
+
+/**@brief Function for application main entry.
+ */
+int main(void)
+{
+    uint32_t err_code;
+	uint8_t index = 0;
+
+    // Initialize.
+    log_init();
+    timers_init();
+    // Start internal LFCLK XTAL oscillator - it is needed by BSP to handle
+    // buttons with the use of APP_TIMER 
+    clock_initialization();
+    gpio_init();
+    power_management_init();
+    init_fstorage();
+    
+    ble_init();
+	wdt_init();
+    
+    NRF_LOG_INFO("Hello USB!");
+    NRF_LOG_FLUSH();
+    usb21_init();
+
+    ikv_spim_init();
+    lcm_init();
+    
+    if(option_pin_mode0 == true)
+    saadc_init();
+		
+    // Start execution.
+    NRF_LOG_INFO("Secux started");
+    application_timers_start();
+    //advertising_start();
+
+    // Enter main loop.
+    for (;;)
+    {
+        idle_state_handle();
+    }
+}
+
+
+/**
+ * @}
+ */
