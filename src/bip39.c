213c0e804e68d1f37dea1addcc78cc31e15b81f5
RainDay <dalizznokia123@gmail.com>
2019-10-11 17:11:11 +0800

    v1

--- src/bip39.c
+++ src/bip39.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include "nrf_drv_rng.h"
 #include "nrf_delay.h"
+#include "nrf_crypto.h"
 #include "nrf_crypto_hmac.h"
 #include "nrf_crypto_hash.h"
 #include "pkcs5.h"
@@ -18,24 +19,28 @@ static nrf_crypto_hmac_context_t m_context;
 
 uint8_t random_vector_generate(uint8_t * p_buff, uint8_t size)
 {
-    uint8_t  available;
-
-    nrf_drv_rng_bytes_available(&available);
-    uint8_t length = MIN(size, available);
-    nrf_drv_rng_rand(p_buff, length);
-    return length;
+    ret_code_t ret_code = NRF_SUCCESS;
+    ret_code = nrf_crypto_rng_vector_generate(p_buff, size);
+    if (ret_code != NRF_SUCCESS)
+    {
+        NRF_LOG_INFO("nrf_crypto_rng_vector_generate failed. (%d)", ret_code);
+    }
+    return ret_code;
 }
 
 void generate_random_bits(uint8_t* random_buff, uint8_t RANDOM_BUFF_SIZE, nrf_crypto_hash_context_t hash_context){
-	uint8_t length = 0;
+	ret_code_t ret_code = NRF_SUCCESS;
 	nrf_crypto_hash_sha256_digest_t     m_digest;
 	const size_t m_digest_len = NRF_CRYPTO_HASH_SIZE_SHA256;
 	size_t digest_len = m_digest_len;
 	
-	while(length != RANDOM_BUFF_SIZE){
+	do
+    {
 		nrf_delay_ms(10);
-		length = random_vector_generate(random_buff, RANDOM_BUFF_SIZE);
-	}	
+		ret_code = random_vector_generate(random_buff, RANDOM_BUFF_SIZE);
+	}
+    while(ret_code != NRF_SUCCESS);
+
 	//for(int i = 0; i < RANDOM_BUFF_SIZE; i++)		
 	//		random_buff[i] = 7 * 16 + 15;
 
