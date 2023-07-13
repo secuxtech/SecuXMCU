91554a7a49dd69502d0ad19f062cdf24a7cc627a
angelcet <coolet.lin@gmail.com>
2023-07-13 10:50:17 +0800

    Add enum and define protection

--- inc/LCD/EPD.h
+++ inc/LCD/EPD.h
@@ -1,5 +1,8 @@
+#ifndef EPD_H
+#define EPD_H
+
 #include "nrf_drv_spi.h"
-#include "nrf_gpio.h"
+// #include "nrf_gpio.h"
 //#define PIN_RESET 22
 //#define PIN_BUSY  25
 #define AMD_HEIGHT  64
@@ -9,6 +12,17 @@
 #define AMD_DISPLAY_BOUNDARY_Y_MIN  3
 #define AMD_DISPLAY_BOUNDARY_Y_MAX  67
 
+enum SD_COLOR{
+    SD_COLOR_NORMAL = 0,
+    SD_COLOR_INVERT = 1
+};
+
+enum SD_GRAYSCALE{
+    SD_GRAYSCALE_OFF = 0,
+    SD_GRAYSCALE_ON = 1
+};
+
+
 void SUB_DISPLAY_FLASH(nrf_drv_spi_t const * const p_instance,uint8_t color);
 void SUB_DISPLAY_DPA(nrf_drv_spi_t const * const p_instance,const unsigned char * p_rx_buffer,uint8_t color,uint8_t  grayscale );
 void SUB_DISPLAY_INIT(nrf_drv_spi_t const * const p_instance);
@@ -16,5 +30,4 @@ uint8_t SUB_DISPLAY_AREA_Pattern(nrf_drv_spi_t const * const p_instance,const un
 uint8_t SUB_GET_TEMPERATURE(nrf_drv_spi_t const * const p_instance);
 
 
-
-
+#endif
